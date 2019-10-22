6d18bb64e6db18bc241845729369828d3fd5a951
ifjso <ojsboy@hotmail.com>
2019-10-22 22:46:54 +0900

    url 수정

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const logger = require('morgan');
 const OAuth2Service = require('./services/OAuth2Service.js');
@@ -8,8 +9,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.all('/oauth/token', OAuth2Service.tokenHandler);
-app.use(OAuth2Service.authenticateHandler);
+// app.get('/oauth/authorize', OAuth2Service.authorize); // TODO login page
+app.post('/oauth/authorize', OAuth2Service.authorize);
+app.post('/oauth/token', OAuth2Service.token);
+
+// app.get('/oauth/authorize', OAuth2Service.authorize);
+app.use(OAuth2Service.authenticate);
 
 app.get('/', (req, res) => res.send('Congratulations, you are in a secret area!'));
 
