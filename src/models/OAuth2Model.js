397e1b841d0ffae01c501f32eba927dcd126a67a
ifjso <ojsboy@hotmail.com>
2019-10-22 09:53:28 +0900

    model required method 구현

--- src/models/OAuth2Model.js
+++ src/models/OAuth2Model.js
@@ -12,41 +12,14 @@ const formats = {
 class OAuth2Model {
   constructor() {
     this.redisClient = new Redis();
-    this.getAuthorizationCode = this.getAuthorizationCode.bind(this);
-    this.saveAuthorizationCode = this.saveAuthorizationCode.bind(this);
     this.getAccessToken = this.getAccessToken.bind(this);
-    this.getClient = this.getClient.bind(this);
     this.getRefreshToken = this.getRefreshToken.bind(this);
+    this.getAuthorizationCode = this.getAuthorizationCode.bind(this);
+    this.getClient = this.getClient.bind(this);
     this.saveToken = this.saveToken.bind(this);
-  }
-
-  async saveAuthorizationCode(code, client, user) {
-    code.client = {
-      id: client.id
-    };
-    code.uesr = {
-      id: user.id
-    };
-
-    const data = { ...code };
-
-    await this.redisClient.hmset(fmt(formats.code, code.authorizationCode), code);
-
-    return data;
-  }
-
-  async getAuthorizationCode(authorizationCode) {
-    const code = await this.redisClient.hgetall(fmt(formats.code, authorizationCode));
-
-    if (!code || code.code !== authorizationCode) {
-      return;
-    }
-
-    debug('getAuthorizationCode: sent authorization code successfully');
-
-    return {
-      ...code
-    };
+    this.saveAuthorizationCode = this.saveAuthorizationCode.bind(this);
+    this.revokeToken = this.revokeToken.bind(this);
+    this.revokeAuthorizationCode = this.revokeAuthorizationCode.bind(this);
   }
 
   async getAccessToken(bearerToken) {
@@ -60,6 +33,32 @@ class OAuth2Model {
 
     return {
       ...token
+    };
+  }
+
+  async getRefreshToken(bearerToken) {
+    const token = await this.redisClient.hgetall(fmt(formats.token, bearerToken));
+
+    if (!token || token.accessToken !== bearerToken) {
+      return;
+    }
+
+    return {
+      ...token
+    };
+  }
+
+  async getAuthorizationCode(authorizationCode) {
+    const code = await this.redisClient.hgetall(fmt(formats.code, authorizationCode));
+
+    if (!code || code.code !== authorizationCode) {
+      return;
+    }
+
+    debug('getAuthorizationCode: sent authorization code successfully');
+
+    return {
+      ...code
     };
   }
 
@@ -78,18 +77,6 @@ class OAuth2Model {
     };
   }
 
-  async getRefreshToken(bearerToken) {
-    const token = await this.redisClient.hgetall(fmt(formats.token, bearerToken));
-
-    if (!token || token.accessToken !== bearerToken) {
-      return;
-    }
-
-    return {
-      ...token
-    };
-  }
-
   async saveToken(token, client, user) {
     const pipe = this.redisClient.pipeline();
     token.clientId = client.clientId;
@@ -105,6 +92,35 @@ class OAuth2Model {
         debug('saveToken: token %s saved successfully', token);
       });
     return data;
+  }
+
+  async saveAuthorizationCode(code, client, user) {
+    code.client = {
+      id: client.id
+    };
+    code.uesr = {
+      id: user.id
+    };
+
+    const data = { ...code };
+
+    await this.redisClient.hmset(fmt(formats.code, code.authorizationCode), code);
+
+    return data;
+  }
+
+  async revokeToken(token) {
+    const result = await this.redisClient.del(fmt(formats.token, token.refreshToken));
+    return result !== 0;
+  }
+
+  async revokeAuthorizationCode(code) {
+    const result = await this.redisClient.del(fmt(formats.code, code.authorizationCode));
+    return result !== 0;
+  }
+
+  async verifyScope(accessToken, scope) {
+    return true;
   }
 }
 
