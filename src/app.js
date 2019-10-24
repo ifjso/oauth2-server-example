94e18f00228a548b9d489ce49994ea7e6c1639d6
ifjso <ojsboy@hotmail.com>
2019-10-24 23:29:42 +0900

    서버 시작 코드 정리

--- src/app.js
+++ src/app.js
@@ -1,55 +1,56 @@
 import express from 'express';
 import http from 'http';
-import Debug from 'debug';
+import debug from 'debug';
 import config from './config';
 import loaders from './loaders';
 
-const debug = Debug('app');
+const log = debug('app:log');
+const logError = debug('app:error');
 
-// const startServer = () => {
-const app = express();
+const startServer = () => {
+  const app = express();
 
-loaders({ app });
+  loaders(app);
 
-const port = config.port || 3000;
-app.set('port', port);
+  const port = config.port || 3000;
+  app.set('port', port);
 
-const server = http.createServer(app);
+  const server = http.createServer(app);
 
-const errorHandler = (error) => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
+  server.listen(port);
+  server.on('error', handleError);
+  server.on('listening', handleListening);
 
-  const bind = typeof port === 'string'
-    ? `Pipe ${port}`
-    : `Port ${port}`;
-
-  switch (error.code) {
-    case 'EACCES':
-      console.error(`${bind} requires elevated privileges`);
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(`${bind} is already in use`);
-      process.exit(1);
-      break;
-    default:
+  function handleError(error) {
+    if (error.syscall !== 'listen') {
       throw error;
+    }
+
+    const bind = typeof port === 'string'
+      ? `Pipe ${port}`
+      : `Port ${port}`;
+
+    switch (error.code) {
+      case 'EACCES':
+        logError(`${bind} requires elevated privileges`);
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        logError(`${bind} is already in use`);
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+
+  function handleListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+      ? `pipe ${addr}`
+      : `port ${addr.port}`;
+    log(`Listening on ${bind}`);
   }
 };
 
-const listeningHandler = () => {
-  const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? `pipe ${addr}`
-    : `port ${addr.port}`;
-  debug(`Listening on ${bind}`);
-};
-
-server.listen(port);
-server.on('error', errorHandler);
-server.on('listening', listeningHandler);
-// };
-
-// startServer();
+startServer();
